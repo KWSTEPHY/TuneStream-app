eb789e7853b2d8bcf5858059088641126ca473b8
stephy <stephshiro35@gmail.com>
2024-09-24 11:55:24 +0300

    add background and install dependencies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,6 @@
 
 import React, { useState, useRef, useEffect } from "react";
-import {
-  MdKeyboardDoubleArrowLeft,
-  MdKeyboardDoubleArrowRight,
-} from "react-icons/md";
+import { MdKeyboardDoubleArrowLeft, MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { AiOutlineClose } from "react-icons/ai"; // Import close icon
 
 const Home = ({ searchResults }) => {
@@ -11,18 +8,16 @@ const Home = ({ searchResults }) => {
   const [playId, setPlayId] = useState("");
   const [playType, setPlayType] = useState("");
 
-  const albumListRef = useRef(null);
-  const playlistListRef = useRef(null);
   const trackListRef = useRef(null);
   const artistListRef = useRef(null);
+  const albumListRef = useRef(null);
+  const playlistListRef = useRef(null);
   const iframeRef = useRef(null); // Ref for iframe
-
 
   const scrollAmount = 600;
   const scrollDelay = 5000;
   const resumeDelay = 15000;
   let currentPosition = 0;
-
 
   const scrollAnimeList = (listRef) => {
     setInterval(function () {
@@ -50,15 +45,17 @@ const Home = ({ searchResults }) => {
     scrollAnimeList(playlistListRef);
   }, []);
 
-
   useEffect(() => {
     if (isPlaying && iframeRef.current) {
       iframeRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [isPlaying]);
 
-
- 
+  const handlePlay = (category, play_id) => {
+    setIsPlaying(true);
+    setPlayId(play_id);
+    setPlayType(category);
+  };
 
   const handleManualScroll = (listRef, direction) => {
     clearInterval(scrollAnimeList);
@@ -72,69 +69,27 @@ const Home = ({ searchResults }) => {
     // Resume auto-scrolling after a delay
     currentPosition = newPosition;
     setTimeout(() => scrollAnimeList(listRef), resumeDelay);
-
-  const handlePlay = (category, play_id) => {
-    setIsPlaying(true);
-    setPlayId(play_id);
-    setPlayType(category);
   };
-
-
 
   const handleCancel = () => {
     setIsPlaying(false); // Hide the iframe when the cancel button is clicked
-
   };
 
   return (
     <div className="flex flex-col h-screen">
       <div className="flex-1">
-
-        
-
-        {searchResults.albums && searchResults.albums.items.length > 0 && (
-          <div className="p-4">
-            <h2 className="text-2xl text-white font-bold mt-8 mb-4">Albums</h2>
-
         {searchResults.tracks && searchResults.tracks.items.length > 0 && (
           <div className="p-4">
             <h2 className="text-2xl text-white font-bold mb-4">Tracks</h2>
-
             <div className="relative">
               <button
                 id="scroll-left-btn"
                 className="absolute left-0 top-28 z-10 text-white px-2 py-1 rounded-lg shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
-
-                onClick={() => handleManualScroll(albumListRef, "left")}
-
                 onClick={() => handleManualScroll(trackListRef, "left")}
-
               >
                 <MdKeyboardDoubleArrowLeft size={48} />
               </button>
               <ul
-
-                className="overflow-x-hidden overflow-y-none whitespace-nowrap space-x-4"
-                ref={albumListRef}
-              >
-                {searchResults.albums.items.map((album) => (
-                  <li
-                    key={album.id}
-                    className="inline-block text-white/90 bg-black/70 rounded-lg shadow-lg w-64 border-2 border-white-200 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
-                  >
-                    <button onClick={() => handlePlay("album", album?.id)}>
-                      <img
-                        className="h-64 w-full object-cover"
-                        src={album.images[0]?.url || ""}
-                        alt={album.name}
-                      />
-                      <div className="flex-1 m-4 overflow-hidden">
-                        <h3 className="pt-2 text-lg font-bold">
-                          {album.name.length > 20
-                            ? album.name.substring(0, 20) + "..."
-                            : album.name}
-                        </h3>
-
                 className="overflow-x-hidden whitespace-nowrap space-x-4"
                 ref={trackListRef}
               >
@@ -166,7 +121,6 @@ const Home = ({ searchResults }) => {
                             .join(", ")
                             .substring(0, 30)}
                         </p>
-
                       </div>
                     </button>
                   </li>
@@ -175,11 +129,7 @@ const Home = ({ searchResults }) => {
               <button
                 id="scroll-right-btn"
                 className="absolute right-0 top-28 z-10 text-white px-2 py-1 rounded-lg shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
-
-                onClick={() => handleManualScroll(albumListRef, "right")}
-
                 onClick={() => handleManualScroll(trackListRef, "right")}
-
               >
                 <MdKeyboardDoubleArrowRight size={48} />
               </button>
@@ -187,30 +137,111 @@ const Home = ({ searchResults }) => {
           </div>
         )}
 
-
-        {searchResults.playlists && searchResults.playlists.items.length > 0 && (
-          <div className="p-4">
-            <h2 className="text-2xl text-white font-bold mt-8 mb-4">Playlists</h2>
-
         {searchResults.artists && searchResults.artists.items.length > 0 && (
           <div className="p-4">
             <h2 className="text-2xl text-white font-bold mt-8 mb-4">Artists</h2>
-
             <div className="relative">
               <button
                 id="scroll-left-btn"
                 className="absolute left-0 top-28 z-10 text-white px-2 py-1 rounded-lg shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
-
-                onClick={() => handleManualScroll(playlistListRef, "left")}
-
                 onClick={() => handleManualScroll(artistListRef, "left")}
-
               >
                 <MdKeyboardDoubleArrowLeft size={48} />
               </button>
               <ul
                 className="overflow-x-hidden overflow-y-none whitespace-nowrap space-x-4"
+                ref={artistListRef}
+              >
+                {searchResults.artists.items.map((artist) => (
+                  <li
+                    key={artist.id}
+                    className="inline-block text-white rounded-lg shadow-lg w-64 text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
+                  >
+                    <button onClick={() => handlePlay("artist", artist.id)}>
+                      <img
+                        className="h-64 w-64  rounded-full p-8"
+                        src={artist.images[0]?.url || ""}
+                        alt={artist.name}
+                      />
+                      <div className="flex-1 m-4 overflow-hidden">
+                        <h3 className="text-lg font-bold">{artist.name}</h3>
+                      </div>
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              <button
+                id="scroll-right-btn"
+                className="absolute right-0 top-28 z-10 text-white px-2 py-1 rounded-lg shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
+                onClick={() => handleManualScroll(artistListRef, "right")}
+              >
+                <MdKeyboardDoubleArrowRight size={48} />
+              </button>
+            </div>
+          </div>
+        )}
 
+        {searchResults.albums && searchResults.albums.items.length > 0 && (
+          <div className="p-4">
+            <h2 className="text-2xl text-white font-bold mt-8 mb-4">Albums</h2>
+            <div className="relative">
+              <button
+                id="scroll-left-btn"
+                className="absolute left-0 top-28 z-10 text-white px-2 py-1 rounded-lg shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
+                onClick={() => handleManualScroll(albumListRef, "left")}
+              >
+                <MdKeyboardDoubleArrowLeft size={48} />
+              </button>
+              <ul
+                className="overflow-x-hidden overflow-y-none whitespace-nowrap space-x-4"
+                ref={albumListRef}
+              >
+                {searchResults.albums.items.map((album) => (
+                  <li
+                    key={album.id}
+                    className="inline-block text-white/90 bg-black/70 rounded-lg shadow-lg w-64 border-2 border-white-200 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
+                  >
+                    <button onClick={() => handlePlay("album", album?.id)}>
+                      <img
+                        className="h-64 w-full object-cover"
+                        src={album.images[0]?.url || ""}
+                        alt={album.name}
+                      />
+                      <div className="flex-1 m-4 overflow-hidden">
+                        <h3 className="pt-2 text-lg font-bold">
+                          {album.name.length > 20
+                            ? album.name.substring(0, 20) + "..."
+                            : album.name}
+                        </h3>
+                      </div>
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              <button
+                id="scroll-right-btn"
+                className="absolute right-0 top-28 z-10 text-white px-2 py-1 rounded-lg shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
+                onClick={() => handleManualScroll(albumListRef, "right")}
+              >
+                <MdKeyboardDoubleArrowRight size={48} />
+              </button>
+            </div>
+          </div>
+        )}
+
+        {searchResults.playlists && searchResults.playlists.items.length > 0 && (
+          <div className="p-4">
+            <h2 className="text-2xl text-white font-bold mt-8 mb-4">Playlists</h2>
+            <div className="relative">
+              <button
+                id="scroll-left-btn"
+                className="absolute left-0 top-28 z-10 text-white px-2 py-1 rounded-lg shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
+                onClick={() => handleManualScroll(playlistListRef, "left")}
+              >
+                <MdKeyboardDoubleArrowLeft size={48} />
+              </button>
+              <ul
+                className="overflow-x-hidden overflow-y-none whitespace-nowrap space-x-4"
                 ref={playlistListRef}
               >
                 {searchResults.playlists.items.map((playlist) => (
@@ -230,23 +261,6 @@ const Home = ({ searchResults }) => {
                             ? playlist.name.substring(0, 20) + "..."
                             : playlist.name}
                         </h3>
-
-                ref={artistListRef}
-              >
-                {searchResults.artists.items.map((artist) => (
-                  <li
-                    key={artist.id}
-                    className="inline-block text-white rounded-lg shadow-lg w-64 text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
-                  >
-                    <button onClick={() => handlePlay("artist", artist.id)}>
-                      <img
-                        className="h-64 w-64  rounded-full p-8"
-                        src={artist.images[0]?.url || ""}
-                        alt={artist.name}
-                      />
-                      <div className="flex-1 m-4 overflow-hidden">
-                        <h3 className="text-lg font-bold">{artist.name}</h3>
-
                       </div>
                     </button>
                   </li>
@@ -255,11 +269,7 @@ const Home = ({ searchResults }) => {
               <button
                 id="scroll-right-btn"
                 className="absolute right-0 top-28 z-10 text-white px-2 py-1 rounded-lg shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-90 duration-300"
-
                 onClick={() => handleManualScroll(playlistListRef, "right")}
-
-                onClick={() => handleManualScroll(artistListRef, "right")}
-
               >
                 <MdKeyboardDoubleArrowRight size={48} />
               </button>
@@ -267,7 +277,6 @@ const Home = ({ searchResults }) => {
           </div>
         )}
       </div>
-
 
       {/* Render Spotify Iframe if an item is clicked */}
       {isPlaying && (
@@ -282,16 +291,12 @@ const Home = ({ searchResults }) => {
             src={`https://open.spotify.com/embed/${playType}/${playId}`}
             width="100%"
             height="80"
-            frameBorder="0"
             allow="encrypted-media"
           ></iframe>
         </div>
       )}
-
     </div>
   );
 };
 
-
 export default Home;
-
